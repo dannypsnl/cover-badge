60fdd5a20481c378f2dd023f79c7addfec8deb96
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-07-03 07:12:08 +0800

    give different color for different coverage range

--- main.rkt
+++ main.rkt
@@ -14,9 +14,13 @@
    (error 'no-matches "please from `raco cover .` first!"))
  (define status (substring ((compose bytes->string/utf-8 car) matches)
                            24 30))
+ (define status-number (string->number (substring status 0 5)))
  (with-output-to-file "coverage/badge.json"
-   	#:exists 'replace
+   #:exists 'replace
    (thunk (write-json (hasheq 'subject "coverage"
                               'icon "github"
                               'status status
-                              'color "green")))))
+                              'color (cond
+                                       [(status-number . >= . 80.) "green"]
+                                       [(status-number . >= . 50.) "yellow"]
+                                       [else "red"]))))))
