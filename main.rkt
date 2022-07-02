78cd0c68f3f28a5a0851802e2092fdf5f86a9fd8
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-07-03 07:57:15 +0800

    handle the format is not dd.dd%
    for example, dd.d% is valid
    
    ^ bug fix

--- main.rkt
+++ main.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 (require racket/cmdline
          racket/list
+         racket/string
          racket/function
          raco/command-name
          json)
@@ -8,13 +9,12 @@
 (command-line
  #:program (short-program+command-name)
  #:args ()
- (define matches (regexp-match #px"Total Project Coverage: \\d\\d\\.\\d\\d%"
+ (define matches (regexp-match #px"Total Project Coverage: \\d+\\.\\d+%"
                                (open-input-file "coverage/index.html")))
  (when (empty? matches)
    (error 'no-matches "please from `raco cover .` first!"))
- (define status (substring ((compose bytes->string/utf-8 car) matches)
-                           24 30))
- (define status-number (string->number (substring status 0 5)))
+ (define status (substring ((compose bytes->string/utf-8 car) matches) 24))
+ (define status-number (string->number (string-trim status "%")))
  (with-output-to-file "coverage/badge.json"
    #:exists 'replace
    (thunk (write-json (hasheq 'subject "coverage"
