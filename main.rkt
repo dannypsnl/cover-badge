8106fc7207a26061d6fa5897de3f32b6fd5b676c
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-07-03 08:04:08 +0800

    regexp-match simply returns `#f` when no matches

--- main.rkt
+++ main.rkt
@@ -1,6 +1,5 @@
 #lang racket/base
 (require racket/cmdline
-         racket/list
          racket/string
          racket/function
          raco/command-name
@@ -11,7 +10,7 @@
  #:args ()
  (define matches (regexp-match #px"Total Project Coverage: \\d+\\.\\d+%"
                                (open-input-file "coverage/index.html")))
- (when (empty? matches)
+ (when matches
    (error 'no-matches "please from `raco cover .` first!"))
  (define status (substring ((compose bytes->string/utf-8 car) matches) 24))
  (define status-number (string->number (string-trim status "%")))
